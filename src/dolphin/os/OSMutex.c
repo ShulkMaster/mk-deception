24241a9aacbf1d63ec938467a687d20d56094f0a
shulkmaster <shulkmaster@gmail.com>
2026-08-06 14:57:37 -0600

    dolphin OS SDK modules

---
+++ src/dolphin/os/OSMutex.c
@@ -0,0 +1,49 @@
+/* TODO: Missing implementation for retail unit OSMutex.c. */
+
+void *OSInitMutex(void)
+{
+    /* TODO: Missing canonical function implementation. */
+    return 0;
+}
+
+void *OSLockMutex(void)
+{
+    /* TODO: Missing canonical function implementation. */
+    return 0;
+}
+
+void *OSUnlockMutex(void)
+{
+    /* TODO: Missing canonical function implementation. */
+    return 0;
+}
+
+void *__OSUnlockAllMutex(void)
+{
+    /* TODO: Missing canonical function implementation. */
+    return 0;
+}
+
+void *OSTryLockMutex(void)
+{
+    /* TODO: Missing canonical function implementation. */
+    return 0;
+}
+
+void *OSInitCond(void)
+{
+    /* TODO: Missing canonical function implementation. */
+    return 0;
+}
+
+void *OSWaitCond(void)
+{
+    /* TODO: Missing canonical function implementation. */
+    return 0;
+}
+
+void *OSSignalCond(void)
+{
+    /* TODO: Missing canonical function implementation. */
+    return 0;
+}
---
+++ src/dolphin/os/OSReset.c
@@ -0,0 +1,43 @@
+/* TODO: Missing implementation for retail unit OSReset.c. */
+
+void *OSRegisterResetFunction(void)
+{
+    /* TODO: Missing canonical function implementation. */
+    return 0;
+}
+
+void *__OSCallResetFunctions(void)
+{
+    /* TODO: Missing canonical function implementation. */
+    return 0;
+}
+
+void *Reset(void)
+{
+    /* TODO: Missing canonical function implementation. */
+    return 0;
+}
+
+void *KillThreads(void)
+{
+    /* TODO: Missing canonical function implementation. */
+    return 0;
+}
+
+void *__OSDoHotReset(void)
+{
+    /* TODO: Missing canonical function implementation. */
+    return 0;
+}
+
+void *OSResetSystem(void)
+{
+    /* TODO: Missing canonical function implementation. */
+    return 0;
+}
+
+void *OSGetResetCode(void)
+{
+    /* TODO: Missing canonical function implementation. */
+    return 0;
+}
