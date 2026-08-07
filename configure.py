f30ee5daad9339a4de6a640864b83f1b62321e71
shulkmaster <shulkmaster@gmail.com>
2026-08-07 16:08:33 -0600

    Correct RenderWare texture compiler mode

--- configure.py
+++ configure.py
@@ -1193,7 +1193,7 @@ config.libs = [
                 NonMatching,
                 "rwcore.a/batextur.obj",
                 source="rw/batextur.c",
-                extra_cflags=["-opt", "off", "-O0", "-inline", "off"],
+                extra_cflags=["-O4,s", "-opt", "off", "-inline", "off"],
             ),
             # -opt off clears inherited -O4,p from cflags_base before per-TU level
             Object(Matching, "rwcore.a/osintf.obj", source="rw/osintf.c",
