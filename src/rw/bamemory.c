d0559e6f5195c603548e16450e0bdda817a7b024
shulkmaster <shulkmaster@gmail.com>
2026-08-10 13:37:18 -0600

    stream and memory ops

--- src/rw/bamemory.c
+++ src/rw/bamemory.c
@@ -1,115 +1,377 @@
-/* TODO: Missing implementation for retail unit bamemory.c. */
+#include "libmkparticle/rw_engine.h"
+#include "runtime/cstring.h"
+#include "rw/rwfreelist.h"
 
-void *_rwFreeListEnable(void)
+typedef struct RwFreeBlock {
+    RwLLLink link;
+    RwUInt8 heap[1];
+} RwFreeBlock;
+
+extern void* malloc(RwUInt32);
+extern void free(void*);
+extern void* realloc(void*, RwUInt32);
+extern void* calloc(RwUInt32, RwUInt32);
+
+static RwFreeList _masterFreeList;
+static RwBool FreeListsEnabled = 1;
+static RwLinkList _freeListList;
+static RwFreeList* _masterFreeListPtr;
+static RwBool freeListModuleOpen;
+
+static RwFreeList* FreeListCreate(RwInt32 entrySize, RwInt32 entriesPerBlock,
+                                  RwUInt32 alignment, RwUInt32 preallocBlocks,
+                                  RwFreeList* freeList, RwUInt32 hint);
+static void _RwFreeListFree(RwFreeList* freeList);
+static RwBool FreeListBlockIsEmpty(const RwUInt8* heap, RwUInt32 heapSize);
+
+void _rwFreeListEnable(RwBool enable)
 {
-    /* TODO: Missing canonical function implementation. */
+    FreeListsEnabled = enable;
+}
+
+static RwBool _rwFreeListModuleOpen(void)
+{
+    RwLLLink* head;
+
+    _freeListList.link.next = &_freeListList.link;
+    head = &_freeListList.link;
+    _freeListList.link.prev = head;
+    freeListModuleOpen = 1;
+    _masterFreeListPtr = FreeListCreate(0x24, 0x10, 0x20, 0,
+                                        &_masterFreeList, 0x40000);
+    if (_masterFreeListPtr == 0) {
+        freeListModuleOpen = 0;
+        return 0;
+    }
+    rwLinkListRemoveLLLink(&_masterFreeListPtr->link);
+    return 1;
+}
+
+static void _rwFreeListModuleClose(void)
+{
+    RwLLLink* link = _freeListList.link.next;
+    RwLLLink* head = &_freeListList.link;
+
+    while (link != head) {
+        RwFreeList* freeList = (RwFreeList*)((RwUInt8*)link - 0x1C);
+        RwFreeListDestroy(freeList);
+        link = _freeListList.link.next;
+        head = &_freeListList.link;
+    }
+    RwFreeListDestroy(_masterFreeListPtr);
+    _masterFreeListPtr = 0;
+    freeListModuleOpen = 0;
+}
+
+
+static RwFreeList* FreeListCreate(RwInt32 entrySize, RwInt32 entriesPerBlock,
+                                  RwUInt32 alignment, RwUInt32 preallocBlocks,
+                                  RwFreeList* freeList, RwUInt32 hint)
+{
+    RwInt32 alignedEntrySize;
+    RwUInt32 heapSize;
+
+    if (!FreeListsEnabled)
+        preallocBlocks = 0;
+    if (alignment == 0)
+        alignment = 0x20;
+    if (freeList == 0) {
+        if (_masterFreeListPtr != 0)
+            freeList = RwEngineInstance->fpFreeListAlloc(_masterFreeListPtr,
+                                                         hint & 0xFF0000);
+        else
+            freeList = RwEngineInstance->fpMalloc(sizeof(RwFreeList),
+                                                  hint & 0xFF0000);
+        if (freeList == 0)
+            return 0;
+        freeList->flags = 2;
+    } else {
+        freeList->flags = 3;
+    }
+
+    alignedEntrySize = (entrySize + alignment - 1) & ~(alignment - 1);
+    heapSize = ((RwUInt32)(entriesPerBlock + 7) & ~7U) >> 3;
+    freeList->entrySize = alignedEntrySize;
+    freeList->entriesPerBlock = entriesPerBlock;
+    freeList->alignment = alignment;
+    freeList->heapSize = heapSize;
+    freeList->blockList.link.next = &freeList->blockList.link;
+    freeList->blockList.link.prev = &freeList->blockList.link;
+
+    while (preallocBlocks != 0) {
+        RwLLLink* newLink;
+        RwFreeBlock* block;
+
+        block = RwEngineInstance->fpMalloc(
+            heapSize + entriesPerBlock * alignedEntrySize + alignment + 7,
+            hint);
+        if (block == 0) {
+            _RwFreeListFree(freeList);
+            return 0;
+        }
+        newLink = &block->link;
+        newLink->prev = 0;
+        newLink->next = 0;
+        rwLinkListAddLLLink(&freeList->blockList, newLink);
+        memset(block->heap, 0, heapSize);
+        --preallocBlocks;
+    }
+    rwLinkListAddLLLink(&_freeListList, &freeList->link);
+    return freeList;
+}
+
+RwFreeList* RwFreeListCreate(RwInt32 entrySize, RwInt32 entriesPerBlock,
+                             RwInt32 alignment, RwUInt32 hint)
+{
+    return FreeListCreate(entrySize, entriesPerBlock, alignment, 1, 0, hint);
+}
+
+RwFreeList* RwFreeListCreateAndPreallocateSpace(
+    RwInt32 entrySize, RwInt32 entriesPerBlock, RwInt32 alignment,
+    RwInt32 preallocBlocks, RwFreeList* freeList, RwUInt32 hint)
+{
+    return FreeListCreate(entrySize, entriesPerBlock, alignment, preallocBlocks,
+                          freeList, hint);
+}
+
+static void _RwFreeListFree(RwFreeList* freeList)
+{
+    RwLLLink* link = freeList->blockList.link.next;
+    RwLLLink* head = &freeList->blockList.link;
+
+    while (link != head) {
+        rwLinkListRemoveLLLink(link);
+        RwEngineInstance->fpFree(link);
+        link = freeList->blockList.link.next;
+        head = &freeList->blockList.link;
+    }
+    if (!(freeList->flags & 1)) {
+        if (_masterFreeListPtr == freeList || _masterFreeListPtr == 0)
+            RwEngineInstance->fpFree(freeList);
+        else
+            RwEngineInstance->fpFreeListFree(_masterFreeListPtr, freeList);
+    }
+}
+
+RwBool RwFreeListDestroy(RwFreeList* freeList)
+{
+    rwLinkListRemoveLLLink(&freeList->link);
+    _RwFreeListFree(freeList);
+    return 1;
+}
+
+
+void* _rwFreeListAllocReal(RwFreeList* freeList, RwUInt32 hint)
+{
+    RwUInt8* result = 0;
+    RwUInt32 heapSize = freeList->heapSize;
+    RwLLLink* link = freeList->blockList.link.next;
+    RwLLLink* head = &freeList->blockList.link;
+    while (link != head && result == 0) {
+        RwUInt8* heap = ((RwFreeBlock*)link)->heap;
+        RwUInt32 remaining = freeList->entriesPerBlock;
+        RwUInt32 byteIndex = 0;
+        while (byteIndex < heapSize) {
+            RwUInt8 byte = heap[byteIndex];
+            if (byte != 0xFF) {
+                RwUInt32 bitIndex = 0;
+                while (bitIndex < 8 && remaining != 0) {
+                    RwUInt8 mask = (RwUInt8)(0x80 >> bitIndex);
+                    if (!(byte & mask)) {
+                        RwUInt8* base;
+                        heap[byteIndex] =
+                            (RwUInt8)(heap[byteIndex] | mask);
+                        base = (RwUInt8*)link + heapSize +
+                               freeList->alignment + 7;
+                        base = (RwUInt8*)((unsigned long)base &
+                                         ~(freeList->alignment - 1));
+                        result = base + freeList->entrySize *
+                                        (byteIndex * 8 + bitIndex);
+                        break;
+                    }
+                    ++bitIndex;
+                    --remaining;
+                }
+            } else {
+                remaining -= 8;
+            }
+            if (result == 0)
+                ++byteIndex;
+            else
+                break;
+        }
+        link = link->next;
+    }
+
+    if (result == 0) {
+        RwLLLink* newLink;
+        RwFreeBlock* block;
+        RwUInt8* base;
+        block = RwEngineInstance->fpMalloc(
+            freeList->alignment +
+                (heapSize + freeList->entriesPerBlock * freeList->entrySize) + 7,
+            hint);
+        if (block == 0)
+            return 0;
+        memset(block->heap, 0, heapSize);
+        newLink = &block->link;
+        rwLinkListAddLLLink(&freeList->blockList, newLink);
+        block->heap[0] = 0x80;
+        base = (RwUInt8*)block + heapSize + freeList->alignment + 7;
+        base = (RwUInt8*)((unsigned long)base &
+                         ~(freeList->alignment - 1));
+        result = base;
+    }
+    return result;
+}
+
+static RwBool FreeListBlockIsEmpty(const RwUInt8* heap, RwUInt32 heapSize)
+{
+    RwInt32 sum = 0;
+    RwInt32 i;
+    for (i = 0; i < heapSize; ++i)
+        sum += heap[i];
+    return sum == 0;
+}
+
+
+RwFreeList* _rwFreeListFreeReal(RwFreeList* freeList, void* entry)
+{
+    RwUInt32 heapSize = freeList->heapSize;
+    RwLLLink* link = freeList->blockList.link.next;
+    RwLLLink* head = &freeList->blockList.link;
+    while (link != head) {
+        RwUInt8* rawBase = ((RwFreeBlock*)link)->heap + heapSize;
+        if ((RwUInt8*)entry >= rawBase &&
+            (RwUInt8*)entry <= rawBase + freeList->entriesPerBlock * freeList->entrySize) {
+            RwUInt32 index = ((RwUInt8*)entry - rawBase) /
+                             (RwUInt32)freeList->entrySize;
+            RwUInt32 byteIndex = index >> 3;
+            RwUInt8 mask = (RwUInt8)(0x80 >> (index - byteIndex * 8));
+            RwUInt8* heap = ((RwFreeBlock*)link)->heap;
+            heap[byteIndex] &= (RwUInt8)~mask;
+            if ((freeList->flags & 2) &&
+                FreeListBlockIsEmpty(heap, heapSize)) {
+                rwLinkListRemoveLLLink(link);
+                RwEngineInstance->fpFree(link);
+            }
+            return freeList;
+        }
+        link = link->next;
+    }
     return 0;
 }
 
-void *_rwFreeListModuleOpen(void)
+RwInt32 RwFreeListPurge(RwFreeList* freeList)
 {
-    /* TODO: Missing canonical function implementation. */
-    return 0;
+    RwInt32 freed = 0;
+    RwUInt32 heapSize = freeList->heapSize;
+    RwLLLink* link = freeList->blockList.link.next;
+    RwLLLink* next;
+    RwLLLink* head = &freeList->blockList.link;
+    while (link != head) {
+        RwUInt8* heap = ((RwFreeBlock*)link)->heap;
+        rwLinkListRemoveLLLink(link);
+        next = link->next;
+        if (FreeListBlockIsEmpty(heap, heapSize)) {
+            RwEngineInstance->fpFree(link);
+            ++freed;
+        } else {
+            rwLinkListAddLLLink(&freeList->blockList, link);
+        }
+        link = next;
+    }
+    return freed * freeList->entrySize;
 }
 
-void *_rwFreeListModuleClose(void)
+
+RwFreeList* RwFreeListForAllUsed(RwFreeList* freeList,
+                                 RwFreeListCallBack callback, void* data)
 {
-    /* TODO: Missing canonical function implementation. */
-    return 0;
+    RwUInt32 heapSize = freeList->heapSize;
+    RwLLLink* link = freeList->blockList.link.next;
+    RwLLLink* head = &freeList->blockList.link;
+    while (link != head) {
+        RwLLLink* next;
+        RwUInt8* blockHeap = ((RwFreeBlock*)link)->heap;
+        RwUInt8* heap = RwEngineInstance->fpMalloc(heapSize, 0x10000);
+        RwUInt32 byteIndex;
+        if (heap == 0)
+            return 0;
+        memcpy(heap, blockHeap, heapSize);
+        next = link->next;
+        for (byteIndex = 0; byteIndex < heapSize; ++byteIndex) {
+            RwUInt32 byte = heap[byteIndex];
+            if (byte != 0) {
+                RwUInt32 bitIndex;
+                for (bitIndex = 0; bitIndex < 8; ++bitIndex) {
+                    RwUInt8 mask = (RwUInt8)(0x80 >> bitIndex);
+                    if (byte & mask) {
+                        RwUInt8* base = (RwUInt8*)link + heapSize +
+                                        freeList->alignment + 7;
+                        void* entry;
+                        base = (RwUInt8*)((unsigned long)base &
+                                         ~(freeList->alignment - 1));
+                        entry = base + freeList->entrySize *
+                                       (byteIndex * 8 + bitIndex);
+                        callback(entry, data);
+                    }
+                }
+            }
+        }
+        RwEngineInstance->fpFree(heap);
+        link = next;
+    }
+    return freeList;
 }
 
-void *FreeListCreate(void)
+RwInt32 RwFreeListPurgeAllFreeLists(void)
 {
-    /* TODO: Missing canonical function implementation. */
-    return 0;
+    RwInt32 total = 0;
+    RwLLLink* link = _freeListList.link.next;
+    RwLLLink* head = &_freeListList.link;
+    while (link != head) {
+        RwFreeList* freeList = (RwFreeList*)((RwUInt8*)link - 0x1C);
+        RwInt32 purged = RwFreeListPurge(freeList);
+        if (purged > 0)
+            total += purged;
+        link = link->next;
+    }
+    return total;
 }
 
-void *RwFreeListCreate(void)
+static void* HMalloc(RwUInt32 size, RwUInt32 hint)
 {
-    /* TODO: Missing canonical function implementation. */
-    return 0;
+    return malloc(size);
 }
 
-void *RwFreeListCreateAndPreallocateSpace(void)
+static void* HRealloc(void* memory, RwUInt32 size, RwUInt32 hint)
 {
-    /* TODO: Missing canonical function implementation. */
-    return 0;
+    return realloc(memory, size);
 }
 
-void *_RwFreeListFree(void)
+static void* HCalloc(RwUInt32 count, RwUInt32 size, RwUInt32 hint)
 {
-    /* TODO: Missing canonical function implementation. */
-    return 0;
+    return calloc(count, size);
 }
 
-void *RwFreeListDestroy(void)
+
+RwBool _rwMemoryOpen(const RwMemoryFunctions* functions)
 {
-    /* TODO: Missing canonical function implementation. */
-    return 0;
+    if (!_rwFreeListModuleOpen())
+        return 0;
+    if (functions != 0) {
+        *(RwMemoryFunctions*)&RwEngineInstance->fpMalloc = *functions;
+    } else {
+        RwEngineInstance->fpMalloc = HMalloc;
+        RwEngineInstance->fpFree = free;
+        RwEngineInstance->fpRealloc = HRealloc;
+        RwEngineInstance->fpCalloc = HCalloc;
+    }
+    return 1;
 }
 
-void *_rwFreeListAllocReal(void)
+void _rwMemoryClose(void)
 {
-    /* TODO: Missing canonical function implementation. */
-    return 0;
-}
-
-void *FreeListBlockIsEmpty(void)
-{
-    /* TODO: Missing canonical function implementation. */
-    return 0;
-}
-
-void *_rwFreeListFreeReal(void)
-{
-    /* TODO: Missing canonical function implementation. */
-    return 0;
-}
-
-void *RwFreeListPurge(void)
-{
-    /* TODO: Missing canonical function implementation. */
-    return 0;
-}
-
-void *RwFreeListForAllUsed(void)
-{
-    /* TODO: Missing canonical function implementation. */
-    return 0;
-}
-
-void *RwFreeListPurgeAllFreeLists(void)
-{
-    /* TODO: Missing canonical function implementation. */
-    return 0;
-}
-
-void *HMalloc(void)
-{
-    /* TODO: Missing canonical function implementation. */
-    return 0;
-}
-
-void *HRealloc(void)
-{
-    /* TODO: Missing canonical function implementation. */
-    return 0;
-}
-
-void *HCalloc(void)
-{
-    /* TODO: Missing canonical function implementation. */
-    return 0;
-}
-
-void *_rwMemoryOpen(void)
-{
-    /* TODO: Missing canonical function implementation. */
-    return 0;
-}
-
-void *_rwMemoryClose(void)
-{
-    /* TODO: Missing canonical function implementation. */
-    return 0;
+    _rwFreeListModuleClose();
 }
--- src/rw/baresour.c
+++ src/rw/baresour.c
@@ -1,49 +1,256 @@
-/* TODO: Missing implementation for retail unit baresour.c. */
+#include "libmkparticle/rw_engine.h"
+#include "rw/rwcore_types.h"
+#include "rw/rwerror.h"
+#include "rw/rwfreelist.h"
+#include "rw/rwresources.h"
 
-void *ResourcesInit(void)
-{
-    /* TODO: Missing canonical function implementation. */
+typedef struct RwResHeap RwResHeap;
+
+typedef struct RwResourcesGlobals {
+    RwUInt32 arenaSize;
+    RwUInt32 arenaUsage;
+    RwUInt32 arenaReusage;
+    RwResHeap* arena;
+    RwLinkList entriesA;
+    RwLinkList entriesB;
+    RwLLLink* activeList;
+    RwLLLink* allocList;
+} RwResourcesGlobals;
+
+RwModuleInfo resourcesModule;
+
+extern RwBool _rwResHeapInit(RwResHeap* heap, RwUInt32 size);
+extern RwBool _rwResHeapClose(RwResHeap* heap);
+extern void _rwResHeapFree(void* memory);
+extern void* _rwResHeapAlloc(RwResHeap* heap, RwUInt32 size);
+
+
+
+
+static RwResourcesGlobals* ResourcesInit(RwResourcesGlobals* globals,
+                                         RwUInt32 size) {
+    if (size != 0) {
+        globals->arena = RwEngineInstance->fpMalloc(size, 0x4040B);
+        if (globals->arena == 0) {
+            RwError error;
+
+            error.pluginID = 1;
+            error.errorCode = _rwerror(0x80000013, size);
+            RwErrorSet(&error);
+            return 0;
+        }
+        if (!_rwResHeapInit(globals->arena, size)) {
+            RwError error;
+
+            RwEngineInstance->fpFree(globals->arena);
+            error.pluginID = 1;
+            error.errorCode = _rwerror(0xC, 0);
+            RwErrorSet(&error);
+            return 0;
+        }
+    } else {
+        globals->arena = 0;
+    }
+    globals->entriesA.link.next = &globals->entriesA.link;
+    globals->entriesA.link.prev = &globals->entriesA.link;
+    globals->entriesB.link.next = &globals->entriesB.link;
+    globals->entriesB.link.prev = &globals->entriesB.link;
+    globals->allocList = &globals->entriesA.link;
+    globals->activeList = &globals->entriesB.link;
+    globals->arenaSize = size;
+    globals->arenaUsage = 0;
+    globals->arenaReusage = 0;
+    return globals;
+}
+
+void* _rwResourcesOpen(void* object, RwInt32 offset, RwInt32 size) {
+    RwUInt32 arenaSize;
+
+    resourcesModule.globalsOffset = offset;
+    arenaSize = RwEngineInstance->resArenaInitSize;
+    if (ResourcesInit(&(*(RwResourcesGlobals*)((RwUInt8*)RwEngineInstance + resourcesModule.globalsOffset)), arenaSize) == 0) {
+        return 0;
+    }
+    resourcesModule.numInstances++;
+    return object;
+}
+
+void* _rwResourcesClose(void* object, RwInt32 offset, RwInt32 size) {
+    RwResourcesEmptyArena();
+    _rwResHeapClose((*(RwResourcesGlobals*)((RwUInt8*)RwEngineInstance + resourcesModule.globalsOffset)).arena);
+    RwEngineInstance->fpFree((*(RwResourcesGlobals*)((RwUInt8*)RwEngineInstance + resourcesModule.globalsOffset)).arena);
+    (*(RwResourcesGlobals*)((RwUInt8*)RwEngineInstance + resourcesModule.globalsOffset)).arena = 0;
+    resourcesModule.numInstances--;
+    return object;
+}
+
+RwBool RwResourcesFreeResEntry(RwResEntry* entry) {
+    if (entry->destroyNotify != 0) {
+        entry->destroyNotify(entry);
+    }
+    if (entry->ownerRef != 0) {
+        *entry->ownerRef = 0;
+    }
+    if (entry->link.next != 0) {
+        entry->link.prev->next = entry->link.next;
+        {
+            RwLLLink* previous = entry->link.prev;
+            entry->link.next->prev = previous;
+        }
+        (*(RwResourcesGlobals*)((RwUInt8*)RwEngineInstance + resourcesModule.globalsOffset)).arenaUsage -= entry->size;
+        _rwResHeapFree(entry);
+    } else {
+        RwEngineInstance->fpFree(entry);
+    }
+    return 1;
+}
+
+
+
+
+void _rwResourcesPurge(void) {
+    RwLLLink* active = (*(RwResourcesGlobals*)((RwUInt8*)RwEngineInstance + resourcesModule.globalsOffset)).activeList;
+    RwLLLink* alloc = (*(RwResourcesGlobals*)((RwUInt8*)RwEngineInstance + resourcesModule.globalsOffset)).allocList;
+
+    if (active->next != active) {
+        if (alloc->next == alloc) {
+            alloc->next = active->next;
+            alloc->prev = active->prev;
+            active->next->prev = alloc;
+            active->prev->next = alloc;
+        } else {
+            RwLLLink* allocLast = alloc->prev;
+            RwLLLink* activeFirst = active->next;
+            RwLLLink* activeLast = active->prev;
+
+            allocLast->next = activeFirst;
+            activeFirst->prev = allocLast;
+            activeLast->next = alloc;
+            alloc->prev = activeLast;
+        }
+        active->next = active;
+        active->prev = active;
+    }
+    (*(RwResourcesGlobals*)((RwUInt8*)RwEngineInstance + resourcesModule.globalsOffset)).allocList = active;
+    (*(RwResourcesGlobals*)((RwUInt8*)RwEngineInstance + resourcesModule.globalsOffset)).activeList = alloc;
+    (*(RwResourcesGlobals*)((RwUInt8*)RwEngineInstance + resourcesModule.globalsOffset)).arenaReusage = 0;
+}
+
+RwResEntry* RwResourcesAllocateResEntry(
+    void* owner, RwResEntry** ownerRef, RwInt32 size,
+    RwResEntryDestroyNotify destroyNotify) {
+    RwResEntry* entry;
+    RwBool exhausted = 0;
+
+    while (exhausted == 0) {
+        entry = _rwResHeapAlloc((*(RwResourcesGlobals*)((RwUInt8*)RwEngineInstance + resourcesModule.globalsOffset)).arena,
+                                size + sizeof(RwResEntry));
+        if (entry != 0) {
+            entry->link.next = (*(RwResourcesGlobals*)((RwUInt8*)RwEngineInstance + resourcesModule.globalsOffset)).allocList->next;
+            entry->link.prev = (*(RwResourcesGlobals*)((RwUInt8*)RwEngineInstance + resourcesModule.globalsOffset)).allocList;
+            (*(RwResourcesGlobals*)((RwUInt8*)RwEngineInstance + resourcesModule.globalsOffset)).allocList->next->prev = &entry->link;
+            (*(RwResourcesGlobals*)((RwUInt8*)RwEngineInstance + resourcesModule.globalsOffset)).allocList->next = &entry->link;
+            entry->owner = owner;
+            entry->size = size;
+            entry->ownerRef = ownerRef;
+            entry->destroyNotify = destroyNotify;
+            (*(RwResourcesGlobals*)((RwUInt8*)RwEngineInstance + resourcesModule.globalsOffset)).arenaUsage += size;
+            if (ownerRef != 0) {
+                *ownerRef = entry;
+            }
+            return entry;
+        }
+        {
+            RwLLLink* tail = (*(RwResourcesGlobals*)((RwUInt8*)RwEngineInstance + resourcesModule.globalsOffset)).activeList->prev;
+
+            if (tail != (*(RwResourcesGlobals*)((RwUInt8*)RwEngineInstance + resourcesModule.globalsOffset)).activeList) {
+                entry = (RwResEntry*)tail;
+                RwResourcesFreeResEntry(entry);
+            } else {
+                tail = (*(RwResourcesGlobals*)((RwUInt8*)RwEngineInstance + resourcesModule.globalsOffset)).allocList->prev;
+                if (tail != (*(RwResourcesGlobals*)((RwUInt8*)RwEngineInstance + resourcesModule.globalsOffset)).allocList) {
+                    entry = (RwResEntry*)tail;
+                    (*(RwResourcesGlobals*)((RwUInt8*)RwEngineInstance + resourcesModule.globalsOffset)).arenaReusage += entry->size;
+                    RwResourcesFreeResEntry(entry);
+                } else {
+                    exhausted = 1;
+                }
+            }
+        }
+    }
+    if (ownerRef != 0) {
+        *ownerRef = 0;
+    }
+    {
+        RwError error;
+
+        error.pluginID = 1;
+        error.errorCode = _rwerror(0xC, size);
+        RwErrorSet(&error);
+    }
     return 0;
 }
 
-void *_rwResourcesOpen(void)
-{
-    /* TODO: Missing canonical function implementation. */
-    return 0;
+RwBool RwResourcesSetArenaSize(RwUInt32 size) {
+    RwResourcesGlobals* globals;
+
+    if (resourcesModule.numInstances == 0) {
+        RwEngineInstance->resArenaInitSize = size;
+        return 1;
+    }
+    globals = &(*(RwResourcesGlobals*)((RwUInt8*)RwEngineInstance + resourcesModule.globalsOffset));
+    globals->arenaSize = size;
+    RwResourcesEmptyArena();
+    _rwResHeapClose(globals->arena);
+    RwEngineInstance->fpFree(globals->arena);
+    globals->arena = RwEngineInstance->fpMalloc(size, 0x3040B);
+    if (globals->arena == 0) {
+        RwError error;
+
+        globals->arenaSize = 0;
+        error.pluginID = 1;
+        error.errorCode = _rwerror(0x80000013, size);
+        RwErrorSet(&error);
+        return 0;
+    }
+    if (!_rwResHeapInit(globals->arena, size)) {
+        RwError error;
+
+        RwEngineInstance->fpFree(globals->arena);
+        error.pluginID = 1;
+        error.errorCode = _rwerror(0xC, 0);
+        RwErrorSet(&error);
+        return 0;
+    }
+    return 1;
 }
 
-void *_rwResourcesClose(void)
-{
-    /* TODO: Missing canonical function implementation. */
-    return 0;
-}
 
-void *RwResourcesFreeResEntry(void)
-{
-    /* TODO: Missing canonical function implementation. */
-    return 0;
-}
 
-void *_rwResourcesPurge(void)
-{
-    /* TODO: Missing canonical function implementation. */
-    return 0;
-}
+RwBool RwResourcesEmptyArena(void) {
+    RwLLLink* link;
+    RwLLLink* end;
 
-void *RwResourcesAllocateResEntry(void)
-{
-    /* TODO: Missing canonical function implementation. */
-    return 0;
-}
+    (*(RwResourcesGlobals*)((RwUInt8*)RwEngineInstance + resourcesModule.globalsOffset)).entriesA.link.prev->next =
+        (*(RwResourcesGlobals*)((RwUInt8*)RwEngineInstance + resourcesModule.globalsOffset)).entriesB.link.next;
+    link = (*(RwResourcesGlobals*)((RwUInt8*)RwEngineInstance + resourcesModule.globalsOffset)).entriesA.link.next;
+    end = &(*(RwResourcesGlobals*)((RwUInt8*)RwEngineInstance + resourcesModule.globalsOffset)).entriesB.link;
+    while (link != end) {
+        RwResEntry* entry = (RwResEntry*)link;
 
-void *RwResourcesSetArenaSize(void)
-{
-    /* TODO: Missing canonical function implementation. */
-    return 0;
-}
-
-void *RwResourcesEmptyArena(void)
-{
-    /* TODO: Missing canonical function implementation. */
-    return 0;
+        link = link->next;
+        RwResourcesFreeResEntry(entry);
+    }
+    (*(RwResourcesGlobals*)((RwUInt8*)RwEngineInstance + resourcesModule.globalsOffset)).entriesA.link.next = &(*(RwResourcesGlobals*)((RwUInt8*)RwEngineInstance + resourcesModule.globalsOffset)).entriesA.link;
+    {
+        RwLLLink* sentinelA = &(*(RwResourcesGlobals*)((RwUInt8*)RwEngineInstance + resourcesModule.globalsOffset)).entriesA.link;
+        (*(RwResourcesGlobals*)((RwUInt8*)RwEngineInstance + resourcesModule.globalsOffset)).entriesA.link.prev = sentinelA;
+    }
+    (*(RwResourcesGlobals*)((RwUInt8*)RwEngineInstance + resourcesModule.globalsOffset)).entriesB.link.next = &(*(RwResourcesGlobals*)((RwUInt8*)RwEngineInstance + resourcesModule.globalsOffset)).entriesB.link;
+    {
+        RwLLLink* sentinelB = &(*(RwResourcesGlobals*)((RwUInt8*)RwEngineInstance + resourcesModule.globalsOffset)).entriesB.link;
+        (*(RwResourcesGlobals*)((RwUInt8*)RwEngineInstance + resourcesModule.globalsOffset)).entriesB.link.prev = sentinelB;
+    }
+    (*(RwResourcesGlobals*)((RwUInt8*)RwEngineInstance + resourcesModule.globalsOffset)).arenaReusage = 0;
+    return 1;
 }
