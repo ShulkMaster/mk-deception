c8d6dfddc343677dea53d64adff8aff7d30a6887
shulkmaster <shulkmaster@gmail.com>
2026-08-25 13:41:58 -0600

    game pads and vi lib

--- include/dolphin/pad.h
+++ include/dolphin/pad.h
@@ -1,6 +1,37 @@
 #ifndef DOLPHIN_PAD_H
 #define DOLPHIN_PAD_H
 
+#define PAD_SPEC_0 0
+#define PAD_SPEC_1 1
+#define PAD_SPEC_2 2
+#define PAD_SPEC_3 3
+#define PAD_SPEC_4 4
+#define PAD_SPEC_5 5
+#define PAD_MOTOR_STOP 0
+#define PAD_MOTOR_RUMBLE 1
+#define PAD_MOTOR_STOP_HARD 2
+#define PAD_CHAN0_BIT 0x80000000UL
+#define PAD_CHAN1_BIT 0x40000000UL
+#define PAD_CHAN2_BIT 0x20000000UL
+#define PAD_CHAN3_BIT 0x10000000UL
+#define PAD_BUTTON_LEFT 0x0001
+#define PAD_BUTTON_RIGHT 0x0002
+#define PAD_BUTTON_DOWN 0x0004
+#define PAD_BUTTON_UP 0x0008
+#define PAD_TRIGGER_Z 0x0010
+#define PAD_TRIGGER_R 0x0020
+#define PAD_TRIGGER_L 0x0040
+#define PAD_BUTTON_A 0x0100
+#define PAD_BUTTON_B 0x0200
+#define PAD_BUTTON_X 0x0400
+#define PAD_BUTTON_Y 0x0800
+#define PAD_BUTTON_MENU 0x1000
+#define PAD_BUTTON_START PAD_BUTTON_MENU
+#define PAD_ERR_NONE 0
+#define PAD_ERR_NO_CONTROLLER -1
+#define PAD_ERR_NOT_READY -2
+#define PAD_ERR_TRANSFER -3
+
 typedef struct PADStatus {
     unsigned short button;
     signed char stickX;
@@ -21,11 +52,25 @@ typedef char PADStatusSizeCheck[sizeof(PADStatus) == 0x0C ? 1 : -1];
 extern "C" {
 #endif
 
-int PADRead(PADStatus* status);
+typedef void (*PADSamplingCallback)(void);
+
+unsigned long PADRead(PADStatus* status);
 void PADClampCircle(PADStatus* status);
 int PADInit(void);
-int PADReset(unsigned int channels);
-void PADControlMotor(int channel, unsigned int command);
+int PADReset(unsigned long channels);
+void PADControlMotor(signed long channel, unsigned long command);
+int PADRecalibrate(unsigned long channels);
+unsigned long PADRead(PADStatus* status);
+void PADSetSamplingRate(unsigned long milliseconds);
+void PADControlAllMotors(const unsigned long* commands);
+void PADSetSpec(unsigned long spec);
+unsigned long PADGetSpec(void);
+int PADGetType(signed long channel, unsigned long* type);
+int PADSync(void);
+void PADSetAnalogMode(unsigned long mode);
+PADSamplingCallback PADSetSamplingCallback(PADSamplingCallback callback);
+int __PADDisableRecalibration(int disable);
+int PADIsBarrel(signed long channel);
 
 #ifdef __cplusplus
 }
--- include/dolphin/vi.h
+++ include/dolphin/vi.h
@@ -3,19 +3,55 @@
 
 #include "dolphin/gx.h"
 
+#define VI_TVMODE(format, scan) (((format) << 2) + (scan))
+#define VI_INTERLACE 0
+#define VI_NON_INTERLACE 1
+#define VI_PROGRESSIVE 2
+#define VI_DEBUG 3
+#define VI_DEBUG_PAL 4
+
+typedef enum VITVMode {
+    VI_TVMODE_NTSC_INT = VI_TVMODE(0, VI_INTERLACE),
+    VI_TVMODE_NTSC_DS = VI_TVMODE(0, VI_NON_INTERLACE),
+    VI_TVMODE_NTSC_PROG = VI_TVMODE(0, VI_PROGRESSIVE),
+    VI_TVMODE_PAL_INT = VI_TVMODE(1, VI_INTERLACE),
+    VI_TVMODE_PAL_DS = VI_TVMODE(1, VI_NON_INTERLACE),
+    VI_TVMODE_EURGB60_INT = VI_TVMODE(5, VI_INTERLACE),
+    VI_TVMODE_EURGB60_DS = VI_TVMODE(5, VI_NON_INTERLACE),
+    VI_TVMODE_MPAL_INT = VI_TVMODE(2, VI_INTERLACE),
+    VI_TVMODE_MPAL_DS = VI_TVMODE(2, VI_NON_INTERLACE),
+    VI_TVMODE_DEBUG_INT = VI_TVMODE(3, VI_INTERLACE),
+    VI_TVMODE_DEBUG_PAL_INT = VI_TVMODE(4, VI_INTERLACE),
+    VI_TVMODE_DEBUG_PAL_DS = VI_TVMODE(4, VI_NON_INTERLACE)
+} VITVMode;
+
+typedef enum VIXFBMode { VI_XFBMODE_SF, VI_XFBMODE_DF } VIXFBMode;
+typedef void (*VIRetraceCallback)(unsigned long retrace_count);
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 void VISetBlack(int black);
 void VIInit(void);
-int VIGetTvFormat(void);
-void VIConfigure(GXRenderModeObj* mode);
+unsigned long VIGetTvFormat(void);
+void VIConfigure(const GXRenderModeObj* mode);
 void VIFlush(void);
 void VIWaitForRetrace(void);
-unsigned int VIGetDTVStatus(void);
-int VIGetNextField(void);
+unsigned long VIGetDTVStatus(void);
+unsigned long VIGetNextField(void);
 void VISetNextFrameBuffer(void* framebuffer);
+VIRetraceCallback VISetPreRetraceCallback(VIRetraceCallback callback);
+VIRetraceCallback VISetPostRetraceCallback(VIRetraceCallback callback);
+void VIConfigurePan(unsigned short x, unsigned short y,
+                    unsigned short width, unsigned short height);
+void VISetNextRightFrameBuffer(void* framebuffer);
+void VISet3D(int enabled);
+unsigned long VIGetRetraceCount(void);
+unsigned long VIGetCurrentLine(void);
+void* VIGetNextFrameBuffer(void);
+void* VIGetCurrentFrameBuffer(void);
+unsigned long VIGetScanMode(void);
 extern GXRenderModeObj GXNtsc480IntDf;
 extern GXRenderModeObj GXPal528IntDf;
 extern GXRenderModeObj GXMpal480IntDf;
