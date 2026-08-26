14328133594a53af149ab8f746315608f434f4d4
shulkmaster <shulkmaster@gmail.com>
2026-08-26 15:09:42 -0600

    events and RTK loops

--- src/dolphin/trk_minnow_dolphin/mainloop.c
+++ src/dolphin/trk_minnow_dolphin/mainloop.c
@@ -1,7 +1,62 @@
-/* TODO: Missing implementation for retail unit mainloop.c. */
+typedef unsigned char u8;
 
-void *TRKNubMainLoop(void)
+typedef struct TRKEvent {
+    int event_type;
+    int event_id;
+    int message_buffer_id;
+} TRKEvent;
+
+extern int TRKGetNextEvent(TRKEvent* event);
+extern void* TRKGetBuffer(int buffer_id);
+extern void TRKDispatchMessage(void* message);
+extern void TRKTargetInterrupt(TRKEvent* event);
+extern void TRKTargetSupportRequest(void);
+extern void TRKDestructEvent(TRKEvent* event);
+extern void TRKGetInput(void);
+extern int TRKTargetStopped(void);
+extern int TRKTargetContinue(void);
+extern u8* gTRKInputPendingPtr;
+
+void TRKNubMainLoop(void)
 {
-    /* TODO: Missing canonical function implementation. */
-    return 0;
+    void* message;
+    TRKEvent event;
+    int shutdown_requested;
+    int new_input;
+
+    shutdown_requested = 0;
+    new_input = 0;
+    while (shutdown_requested == 0) {
+        if (TRKGetNextEvent(&event) != 0) {
+            new_input = 0;
+            switch (event.event_type) {
+            case 0:
+                break;
+            case 2:
+                message = TRKGetBuffer(event.message_buffer_id);
+                TRKDispatchMessage(message);
+                break;
+            case 1:
+                shutdown_requested = 1;
+                break;
+            case 3:
+            case 4:
+                TRKTargetInterrupt(&event);
+                break;
+            case 5:
+                TRKTargetSupportRequest();
+                break;
+            }
+            TRKDestructEvent(&event);
+            continue;
+        }
+        if (new_input == 0 || *gTRKInputPendingPtr != '\0') {
+            new_input = 1;
+            TRKGetInput();
+            continue;
+        }
+        if (TRKTargetStopped() == 0)
+            TRKTargetContinue();
+        new_input = 0;
+    }
 }
--- src/dolphin/trk_minnow_dolphin/nubinit.c
+++ src/dolphin/trk_minnow_dolphin/nubinit.c
@@ -1,19 +1,108 @@
-/* TODO: Missing implementation for retail unit nubinit.c. */
+typedef unsigned char u8;
+typedef unsigned long u32;
+typedef int BOOL;
+typedef int DSError;
 
-void *TRKNubWelcome(void)
+typedef union EndianTest {
+    u8 bytes[4];
+    u32 word;
+} EndianTest;
+
+enum {
+    FALSE = 0,
+    TRUE = 1,
+    DS_NoError = 0,
+};
+
+extern void TRK_board_display(const char* message);
+extern void MWTRACE(int level, const char* format, ...);
+extern void usr_put_initialize(void);
+extern DSError TRKInitializeEventQueue(void);
+extern DSError TRKInitializeMessageBuffers(void);
+extern DSError TRKInitializeDispatcher(void);
+extern DSError InitializeProgramEndTrap(void);
+extern DSError TRKInitializeSerialHandler(void);
+extern DSError TRKTerminateSerialHandler(void);
+extern DSError TRKInitializeTarget(void);
+extern DSError TRKInitializeIntDrivenUART(u32 address, u32 channel, u32 unused,
+                                         volatile u8** input_pending_ptr);
+extern void TRKTargetSetInputPendingPtr(volatile u8* input_pending_ptr);
+
+extern volatile u8* gTRKInputPendingPtr;
+
+BOOL gTRKBigEndian;
+
+static const char welcome_message[] = "MetroTRK for GAMECUBE v2.6";
+static const char initialize_message[] = "Initialize NUB\n";
+
+static inline BOOL TRKInitializeEndian(void);
+
+DSError TRKInitializeNub(void)
 {
-    /* TODO: Missing canonical function implementation. */
-    return 0;
+    DSError error;
+    DSError uart_error;
+
+    error = TRKInitializeEndian();
+
+    MWTRACE(1, initialize_message);
+    if (error == DS_NoError) {
+        usr_put_initialize();
+    }
+    if (error == DS_NoError) {
+        error = TRKInitializeEventQueue();
+    }
+    if (error == DS_NoError) {
+        error = TRKInitializeMessageBuffers();
+    }
+    if (error == DS_NoError) {
+        error = TRKInitializeDispatcher();
+    }
+    InitializeProgramEndTrap();
+    if (error == DS_NoError) {
+        error = TRKInitializeSerialHandler();
+    }
+    if (error == DS_NoError) {
+        error = TRKInitializeTarget();
+    }
+    if (error == DS_NoError) {
+        uart_error = TRKInitializeIntDrivenUART(0xE100, 1, 0, &gTRKInputPendingPtr);
+        TRKTargetSetInputPendingPtr(gTRKInputPendingPtr);
+        if (uart_error != DS_NoError) {
+            error = uart_error;
+        }
+    }
+    return error;
 }
 
-void *TRKTerminateNub(void)
+DSError TRKTerminateNub(void)
 {
-    /* TODO: Missing canonical function implementation. */
-    return 0;
+    TRKTerminateSerialHandler();
+    return DS_NoError;
 }
 
-void *TRKInitializeNub(void)
+void TRKNubWelcome(void)
 {
-    /* TODO: Missing canonical function implementation. */
-    return 0;
+    TRK_board_display(welcome_message);
+}
+
+static inline BOOL TRKInitializeEndian(void)
+{
+    EndianTest endian_test;
+    BOOL error = FALSE;
+
+    gTRKBigEndian = TRUE;
+
+    endian_test.bytes[0] = 0x12;
+    endian_test.bytes[1] = 0x34;
+    endian_test.bytes[2] = 0x56;
+    endian_test.bytes[3] = 0x78;
+
+    if (endian_test.word == 0x12345678) {
+        gTRKBigEndian = TRUE;
+    } else if (endian_test.word == 0x78563412) {
+        gTRKBigEndian = FALSE;
+    } else {
+        error = TRUE;
+    }
+    return error;
 }
