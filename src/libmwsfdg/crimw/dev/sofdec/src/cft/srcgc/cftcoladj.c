3d9441552b39294bfdb2301bc0f4ccf7babff8d2
shulkmaster <shulkmaster@gmail.com>
2026-08-27 15:41:00 -0600

    matrix operations and pixel chroma sampling

--- src/libmwsfdg/crimw/dev/sofdec/src/cft/srcgc/cftcoladj.c
+++ src/libmwsfdg/crimw/dev/sofdec/src/cft/srcgc/cftcoladj.c
@@ -1,19 +1,123 @@
-/* TODO: Missing implementation for retail unit cftcoladj.c. */
+typedef float CFTMtx3D[3][3];
+typedef unsigned char CFTConvTable[256];
 
-void *CFT_MakeMtx3D(void)
+static const float cft_inverse_scale[] = {1.0f};
+
+void CFT_MakeMtx3D(CFTMtx3D left, CFTMtx3D right, CFTMtx3D product)
 {
-    /* TODO: Missing canonical function implementation. */
-    return 0;
+    product[0][0] = left[0][2] * right[2][0] +
+                    (left[0][0] * right[0][0] + left[0][1] * right[1][0]);
+    product[0][1] = left[0][2] * right[2][1] +
+                    (left[0][0] * right[0][1] + left[0][1] * right[1][1]);
+    product[0][2] = left[0][2] * right[2][2] +
+                    (left[0][0] * right[0][2] + left[0][1] * right[1][2]);
+    product[1][0] = left[1][2] * right[2][0] +
+                    (left[1][0] * right[0][0] + left[1][1] * right[1][0]);
+    product[1][1] = left[1][2] * right[2][1] +
+                    (left[1][0] * right[0][1] + left[1][1] * right[1][1]);
+    product[1][2] = left[1][2] * right[2][2] +
+                    (left[1][0] * right[0][2] + left[1][1] * right[1][2]);
+    product[2][0] = left[2][2] * right[2][0] +
+                    (left[2][0] * right[0][0] + left[2][1] * right[1][0]);
+    product[2][1] = left[2][2] * right[2][1] +
+                    (left[2][0] * right[0][1] + left[2][1] * right[1][1]);
+    product[2][2] = left[2][2] * right[2][2] +
+                    (left[2][0] * right[0][2] + left[2][1] * right[1][2]);
 }
 
-void *CFT_MakeInverseMtx3D(void)
+void CFT_MakeInverseMtx3D(CFTMtx3D matrix, CFTMtx3D inverse)
 {
-    /* TODO: Missing canonical function implementation. */
-    return 0;
+    float b = matrix[0][1];
+    float d = matrix[1][0];
+    float f = matrix[1][2];
+    float g = matrix[2][0];
+    float a = matrix[0][0];
+    float e = matrix[1][1];
+    float c = matrix[0][2];
+    float i = matrix[2][2];
+    float h = matrix[2][1];
+    float determinant = h * (c * d) + i * (a * e) + g * (b * f) -
+                        (g * (c * e) + h * (a * f) + i * (b * d));
+
+    inverse[0][0] = cft_inverse_scale[0] * ((e * i - f * h) / determinant);
+    inverse[0][1] = cft_inverse_scale[0] * (-(b * i - c * h) / determinant);
+    inverse[0][2] = cft_inverse_scale[0] * ((b * f - c * e) / determinant);
+    inverse[1][0] = cft_inverse_scale[0] * (-(d * i - f * g) / determinant);
+    inverse[1][1] = cft_inverse_scale[0] * ((a * i - c * g) / determinant);
+    inverse[1][2] = cft_inverse_scale[0] * (-(a * f - c * d) / determinant);
+    inverse[2][0] = cft_inverse_scale[0] * ((d * h - e * g) / determinant);
+    inverse[2][1] = cft_inverse_scale[0] * (-(a * h - b * g) / determinant);
+    inverse[2][2] = cft_inverse_scale[0] * ((a * e - b * d) / determinant);
 }
 
-void *CFT_MakeInvConvTableCustom(void)
+void CFT_MakeInvConvTableCustom(CFTConvTable luma, CFTConvTable chroma_u,
+                                CFTConvTable chroma_v)
 {
-    /* TODO: Missing canonical function implementation. */
-    return 0;
+    int index;
+    int value = 0;
+    int scaled;
+    int divisor;
+    int start;
+    int value_range;
+
+    index = 0;
+    while (index < 16) {
+        luma[index++] = value++;
+        luma[index++] = value++;
+        value++;
+    }
+    for (; index < 176; index += 2) {
+        luma[index] = value;
+        luma[index + 1] = value;
+        value++;
+    }
+    for (; index < 192; index++) {
+        luma[index] = value++;
+    }
+    for (; index < 256; index++) {
+        luma[index] = value < 255 ? value : 255;
+        value += 2;
+    }
+
+    index = 128;
+    value = 128;
+    while (index > 104) {
+        chroma_u[index] = value;
+        chroma_v[index] = value;
+        chroma_u[index - 1] = value;
+        chroma_v[index - 1] = value;
+        chroma_u[index - 2] = value;
+        chroma_v[index - 2] = value;
+        index -= 3;
+        value--;
+    }
+    scaled = index * value;
+    divisor = index;
+    for (; index >= 0; index--) {
+        unsigned char converted = scaled / divisor;
+        chroma_u[index] = converted;
+        chroma_v[index] = converted;
+        scaled -= value;
+    }
+
+    index = 128;
+    value = 128;
+    while (index < 152) {
+        chroma_u[index] = value;
+        chroma_v[index] = value;
+        chroma_u[index + 1] = value;
+        chroma_v[index + 1] = value;
+        chroma_u[index + 2] = value;
+        chroma_v[index + 2] = value;
+        index += 3;
+        value++;
+    }
+    value_range = 255 - value;
+    divisor = 255 - index;
+    start = index;
+    for (; index < 256; index++) {
+        unsigned char converted = value + value_range * (index - start) / divisor;
+        chroma_u[index] = converted;
+        chroma_v[index] = converted;
+    }
 }
--- src/libmwsfdg/crimw/dev/sofdec/src/sfdcore/dct/dct_ac.c
+++ src/libmwsfdg/crimw/dev/sofdec/src/sfdcore/dct/dct_ac.c
@@ -1,19 +1,117 @@
-/* TODO: Missing implementation for retail unit dct_ac.c. */
+double cos(double);
+const char* DCT_GetVerStr(void);
 
-void *dctac_TransDouble(void)
-{
-    /* TODO: Missing canonical function implementation. */
-    return 0;
+static double dctac_i_const[8][8];
+static double dctac_f_const[8][8];
+static const char* dctac_version_dummy;
+
+void dctac_TransDouble(const double* input, double* output,
+                       const double* transform) {
+    double temporary[64];
+    int row;
+    int column;
+
+    for (row = 0; row < 64; row += 8) {
+        int row1 = row + 1;
+        int row2 = row + 2;
+        int row3 = row + 3;
+        int row4 = row + 4;
+        int row5 = row + 5;
+        int row6 = row + 6;
+        int row7 = row + 7;
+
+        for (column = 0; column < 8; column += 2) {
+            double sample0 = input[row];
+            double sample1 = input[row1];
+            double sample2 = input[row2];
+            double sample3 = input[row3];
+            double sample4 = input[row4];
+            double sample5 = input[row5];
+            double sample6 = input[row6];
+            double sample7 = input[row7];
+            double value0 = 0.0;
+            double value1 = 0.0;
+
+            value0 += transform[column] * sample0;
+            value0 += transform[column + 8] * sample1;
+            value0 += transform[column + 16] * sample2;
+            value0 += transform[column + 24] * sample3;
+            value0 += transform[column + 32] * sample4;
+            value0 += transform[column + 40] * sample5;
+            value0 += transform[column + 48] * sample6;
+            value0 += transform[column + 56] * sample7;
+            value1 += transform[column + 1] * sample0;
+            value1 += transform[column + 9] * sample1;
+            value1 += transform[column + 17] * sample2;
+            value1 += transform[column + 25] * sample3;
+            value1 += transform[column + 33] * sample4;
+            value1 += transform[column + 41] * sample5;
+            value1 += transform[column + 49] * sample6;
+            value1 += transform[column + 57] * sample7;
+            temporary[row + column] = value0;
+            temporary[row + column + 1] = value1;
+        }
+    }
+
+    for (column = 0; column < 8; column++) {
+        int column1 = column + 8;
+        int column2 = column + 16;
+        int column3 = column + 24;
+        int column4 = column + 32;
+        int column5 = column + 40;
+        int column6 = column + 48;
+        int column7 = column + 56;
+
+        for (row = 0; row < 8; row += 2) {
+            double sample0 = temporary[column];
+            double sample1 = temporary[column1];
+            double sample2 = temporary[column2];
+            double sample3 = temporary[column3];
+            double sample4 = temporary[column4];
+            double sample5 = temporary[column5];
+            double sample6 = temporary[column6];
+            double sample7 = temporary[column7];
+            double value0 = 0.0;
+            double value1 = 0.0;
+
+            value0 += transform[row] * sample0;
+            value0 += transform[row + 8] * sample1;
+            value0 += transform[row + 16] * sample2;
+            value0 += transform[row + 24] * sample3;
+            value0 += transform[row + 32] * sample4;
+            value0 += transform[row + 40] * sample5;
+            value0 += transform[row + 48] * sample6;
+            value0 += transform[row + 56] * sample7;
+            value1 += transform[row + 1] * sample0;
+            value1 += transform[row + 9] * sample1;
+            value1 += transform[row + 17] * sample2;
+            value1 += transform[row + 25] * sample3;
+            value1 += transform[row + 33] * sample4;
+            value1 += transform[row + 41] * sample5;
+            value1 += transform[row + 49] * sample6;
+            value1 += transform[row + 57] * sample7;
+            output[row * 8 + column] = value0;
+            output[(row + 1) * 8 + column] = value1;
+        }
+    }
 }
 
-void *DCT_AcIdctDouble(void)
-{
-    /* TODO: Missing canonical function implementation. */
-    return 0;
+void DCT_AcIdctDouble(const double input[8][8], double output[8][8]) {
+    dctac_TransDouble(&input[0][0], &output[0][0], &dctac_i_const[0][0]);
 }
 
-void *DCT_AcInit(void)
-{
-    /* TODO: Missing canonical function implementation. */
-    return 0;
+void DCT_AcInit(void) {
+    int row;
+    int column;
+
+    dctac_version_dummy = DCT_GetVerStr();
+    for (row = 0; row < 8; row++) {
+        double scale = row == 0 ? 0.3535533905932738 : 0.5;
+        for (column = 0; column < 8; column++) {
+            double value = scale * cos(
+                0.39269908169872414 * row * (0.5 + column));
+            dctac_i_const[row][column] = value;
+            dctac_f_const[column][row] = value;
+        }
+    }
 }
